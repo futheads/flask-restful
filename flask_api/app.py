319498a51e2f76a6507360e3c996ff44dd7cb8d2
lijianhu <futhead@g2l-service.com>
2019-05-07 18:41:33 +0800

    【A】集成orm框架

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,12 +1,39 @@
-from flask import Flask
-from flask_restplus import Resource, Api
+from flask import Flask, Blueprint
+from flask_api.database import db
+# from flask_api.api.blog.endpoints.posts import ns as blog_posts_namespace
+# from flask_api.api.blog.endpoints.categories import ns as blog_categories_namespace
+from flask_api import config
+from flask_api.api.restplus import api
+
 
 app = Flask(__name__)
-api = Api(app)
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    @staticmethod
-    def get():
-        return {'hello': 'world'}
+def configure_app(flask_app):
+    flask_app.config['SERVER_NAME'] = config.FLASK_SERVER_NAME
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
+
+    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = config.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
+    flask_app.config['RESTPLUS_VALIDATE'] = config.RESTPLUS_VALIDATE
+    flask_app.config['RESTPLUS_MASK_SWAGGER'] = config.RESTPLUS_MASK_SWAGGER
+    flask_app.config['ERROR_404_HELP'] = config.RESTPLUS_ERROR_404_HELP
+
+
+def initialize_app(flask_app):
+    from flask_api.api.blog.endpoints import posts, categories
+    configure_app(flask_app)
+
+    blueprint = Blueprint("api", __name__, url_prefix="/api")
+    api.init_app(blueprint)
+    app.register_blueprint(blueprint)
+
+    db.init_app(flask_app)
+
+
+def main():
+    initialize_app(app)
+    # log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+
+
+main()
