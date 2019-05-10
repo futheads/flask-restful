ec5e949a0e21bc18d92552f49c63c747cd1a5532
lijianhu <futhead@g2l-service.com>
2019-05-10 11:10:57 +0800

    【A】通过supervisor实现服务监控

--- tornado_server.py
+++ tornado_server.py
@@ -1,19 +1,17 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+
 from flask_api.app import app
-
 from flask_api.config import configs
-
 
 if configs["cert"]["cert_key_path"] and configs["cert"]["cert_crt_path"]:
     http_server = HTTPServer(WSGIContainer(app), ssl_options={
         "certfile": configs["cert"]["cert_key_path"],
         "keyfile": configs["cert"]["cert_crt_path"],
     })
-    http_server.listen(443)  # flask默认的端口
+    http_server.listen(443)
 else:
     http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5000)  # flask默认的端口
+    http_server.listen(5000)
     IOLoop.instance().start()
-
