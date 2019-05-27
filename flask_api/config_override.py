9f2407d70ed1f2daedb0b315e279939992d5ff1e
lijianhu <futhead@g2l-service.com>
2019-05-27 12:07:45 +0800

    【A】添加对docker及docker-compose的支持

--- flask_api/config_override.py
+++ flask_api/config_override.py
@@ -6,8 +6,11 @@ configs = {
     "flask_debug": False,
     "flask_server_name": "localhost:5000",
     "qiniu": {
-        "access_key": "RqyZygLm_HLKBa3Spd_8uhR0VOdwcnEPdCNfKmDU",
-        "secret_key": "uXCWBBiqV1R7h14uBcpqGRr5mtzswI_I_9X10Dqc",
-        "bucket_name": "image"
+        "access_key": "",
+        "secret_key": "",
+        "bucket_name": ""
+    },
+    "db": {
+        "redis_url": "redis://redis:6379/1"
     }
 }
