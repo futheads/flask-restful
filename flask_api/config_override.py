39ea24747efb31e9ac35dfb1f8b0f3f77f18457c
lijianhu <futhead@g2l-service.com>
2019-05-27 13:18:26 +0800

    【M】七牛云默认参数不能为空

--- flask_api/config_override.py
+++ flask_api/config_override.py
@@ -6,9 +6,9 @@ configs = {
     "flask_debug": False,
     "flask_server_name": "localhost:5000",
     "qiniu": {
-        "access_key": "",
-        "secret_key": "",
-        "bucket_name": ""
+        "access_key": "access_key",
+        "secret_key": "secret_key",
+        "bucket_name": "bucket_name"
     },
     "db": {
         "redis_url": "redis://redis:6379/1"
